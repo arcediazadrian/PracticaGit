35629eadf953285ea849465262f7475ea1b092cd
Adrian Arce <arce.diaz.adrian@gmail.com>
2017-06-12 09:04:45 -0400

    Configuracion proyecto y clase auto

---
+++ src/Auto.java
@@ -0,0 +1,40 @@
+
+public class Auto {
+	private String marca;
+	private int año;
+	private String modelo;
+	private int cilindrada;
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAño(int año) {
+		this.año = año;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+
+}
